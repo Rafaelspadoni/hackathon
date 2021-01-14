aca6063683cd1e04e6ce25a5e98960bb1cfe91e7
Luiz Emanoel <luizemanoelponce@outlook.com.br>
2021-01-14 16:43:33 -0300

    guarda_vaga_corrigido

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Models\empresa;
 
 
@@ -25,9 +26,22 @@ class EmpresasController extends Controller
     public function guarda_vagas(Request $request)
     {
         $empresa_id = Auth::user()->id;
+        $cargo = $request->cargo;
+        $descricao = $request->descricao;
+        $local_de_trabalho = $request->local_de_trabalho;
+        $tipo_de_contratacao = $request->tipo_de_contratacao;
+        $expiracao = $request->expiracao;
+        $salario = $request->salario;
+        $link_de_vaga = $request->link_de_vaga;
+
         $vaga = new empresa();
-        $salva = $vaga->store_vaga($empresa_id, $request->cargo, $request->descricao, $request->local_de_trabalho, $request->tipo_de_contratacao, $request->expiracao, $request->salario, $request->link_de_vaga);
-        dd($salva); 
+
+        $guarda = $vaga->store_vaga($empresa_id, $cargo, $descricao, $local_de_trabalho, $tipo_de_contratacao, $expiracao, $salario, $link_de_vaga);
+
+        if($guarda){
+
+        }
+
     }
 
 
--- app/Models/empresa.php
+++ app/Models/empresa.php
@@ -12,28 +12,24 @@ class empresa extends Model
     use HasFactory;
 
 
-    public function store_vaga($empresa_id, $cargo, $descricao, $local_de_trabalho, $tipo_de_contratacao, $expiracao, $salario, $link_da_vaga)
+    public function store_vaga($empresa_id , $cargo , $descricao , $local_de_trabalho , $tipo_de_contratacao , $expiracao , $salario , $link_da_vaga)
     {
-        $empresa_id = $empresa_id;
-        $cargo = $cargo;
-        $descricao = $descricao;
-        $local_de_trabalho = $local_de_trabalho;
-        $tipo_de_contratacao = $tipo_de_contratacao;
-        $expiracao = $expiracao;
-        $salario = $salario;
-        $link_da_vaga = $link_da_vaga;
-        DB::transaction(function () {
-            DB::insert('INSERT INTO vagas (empresa_id, cargo, descricao, local_de_trabalho, tipo_de_contratacao, expiracao, salario, link_da_vaga) VALUES (:empresa_id, :cargo, :descricao, :local_de_trabalho, :tipo_de_contratacao, :expiracao, :salario, :link_da_vaga)',
-        ['empresa_id'=> $empresa_id, 
-        'cargo'=> $cargo, 
-        'descricao'=> $descricao, 
-        'local_de_trabalho'=> $local_de_trabalho, 
-        'tipo_de_contratacao'=> $tipo_de_contratacao, 
-        'expiracao'=> $expiracao, 
-        'salario'=> $salario, 
-        'link_de_vaga'=> $link_da_vaga]
-    );
-        });
+    
+    
+        $store = DB::insert(
+        'INSERT INTO vagas (empresa_id , cargo , descricao , local_de_trabalho , tipo_de_contratacao , expiracao , salario , link_da_vaga) VALUES (:empresa_id , :cargo , :descricao , :local_de_trabalho , :tipo_de_contratacao , :expiracao , :salario , :link_da_vaga)',
+        [   'empresa_id' => $empresa_id, 
+            'cargo' => $cargo, 
+            'descricao' => $descricao, 
+            'local_de_trabalho' => $local_de_trabalho, 
+            'tipo_de_contratacao' => $tipo_de_contratacao, 
+            'expiracao' => $expiracao, 
+            'salario' => $salario, 
+            'link_da_vaga' => $link_da_vaga
+        ]);
+
+        return $store;
+
     }
 
 }
